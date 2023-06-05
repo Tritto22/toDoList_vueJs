853a355afeea8e14eba98ce14dbcb0725aab9b8d
Simone <tritto.simone@gmail.com>
2023-06-05 12:02:44 +0200

    add_todo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,48 @@
 const app = new Vue({
     el: '#root',
     data: {
-
+            taskList: [
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'mangiare',
+                    done: false
+                },
+                {
+                    text: 'dormire',
+                    done: false
+                }
+            ],
+            topPriorities: [
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'fare la spesa',
+                    done: false
+                }
+            ],
+            reminder: [
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'fare la spesa',
+                    done: false
+                }
+            ]
     },
     methods: {
 
