f6c871ac7084f5e783ec2e59a6e6d5486ccf551a
Simone <tritto.simone@gmail.com>
2023-06-07 14:40:44 +0200

    end_todo

--- js/main.js
+++ js/main.js
@@ -1,50 +1,71 @@
 const app = new Vue({
     el: '#root',
     data: {
-            taskList: [
-                {
-                    text: 'fare la spesa',
-                    done: false
-                },
-                {
-                    text: 'mangiare',
-                    done: false
-                },
-                {
-                    text: 'dormire',
-                    done: false
-                }
-            ],
-            topPriorities: [
-                {
-                    text: 'fare la spesa',
-                    done: false
-                },
-                {
-                    text: 'fare la spesa',
-                    done: false
-                },
-                {
-                    text: 'fare la spesa',
-                    done: false
-                }
-            ],
-            reminder: [
-                {
-                    text: 'fare la spesa',
-                    done: false
-                },
-                {
-                    text: 'fare la spesa',
-                    done: false
-                },
-                {
-                    text: 'fare la spesa',
-                    done: false
-                }
-            ]
+        taskList: [
+            {
+                text: 'fare la spesa',
+                done: false
+            },
+            {
+                text: 'mangiare',
+                done: false
+            },
+            {
+                text: 'dormire',
+                done: false
+            }
+        ],
+        topPriorities: [
+            {
+                text: 'fare la spesa',
+                done: false
+            },
+            {
+                text: 'fare la spesa',
+                done: false
+            },
+            {
+                text: 'fare la spesa',
+                done: false
+            }
+        ],
+        reminder: [
+            {
+                text: 'fare la spesa',
+                done: false
+            },
+            {
+                text: 'fare la spesa',
+                done: false
+            },
+            {
+                text: 'fare la spesa',
+                done: false
+            }
+        ],
+        newText: '',
+        selectList: document.getElementById('select-list').value
     },
     methods: {
-
+        remove: function(list,i){
+            list.splice(i,1);
+        },
+        add: function(){
+            console.log(this.selectList);
+            switch(this.selectList){
+                case '1':
+                    this.taskList.push({text:this.newText, done:false});
+                    break
+                case '2':
+                    this.topPriorities.push({text:this.newText, done:false});
+                    break
+                case '3':
+                    this.reminder.push({text:this.newText, done:false});
+            }
+            this.newText='';
+        },
+        selectedList: function(){
+            this.selectList= document.getElementById('select-list').value;
+        }
     }
 });
